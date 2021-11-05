9b40fe817dff5cd1ab8a07d63afe3ba15b60f794
liujiachi <5yz_uwwm7iqu5@dingtalk.com>
2021-11-05 15:17:34 +0800

    feat: 项目提交规范

--- src/views/common/404.vue
+++ src/views/common/404.vue
@@ -1,40 +1,41 @@
 <template>
-  <div class="not-found">
-    <h1 class="con-center">
-      <img :src="png404" class="img" />
-      <!-- <p class="err-tit">404</p> -->
-      <p class="err-description">当前访问页面不存在</p>
-      <p class="err-goback" @click="backHome">返回</p>
-    </h1>
-  </div>
+	<div class="not-found">
+		<h1 class="con-center">
+			<img :src="png404" class="img" />
+			<!-- <p class="err-tit">404</p> -->
+			<p class="err-description">当前访问页面不存在</p>
+			<p class="err-goback" @click="backHome">返回</p>
+		</h1>
+	</div>
 </template>
 <script setup>
-import { useRouter } from "vue-router";
-import png404 from "/@assets/common/404.png";
-const router = useRouter();
+import { useRouter } from 'vue-router'
+import png404 from '/@assets/common/404.png'
+const router = useRouter()
 const backHome = () => {
-  router.go(-1);
-};
+	router.go(-1)
+}
+console.log('===')
 </script>
 <style lang="less" scoped>
 .not-found {
-  .con-center {
-    .img {
-      width: 100%;
-    }
-    .err-tit {
-      color: #666;
-      font-weight: bold;
-    }
-    .err-description {
-      margin-top: 20px;
-      text-align: center;
-      font-size: 14px;
-    }
-    .err-goback {
-      text-align: center;
-      color: #666;
-    }
-  }
+	.con-center {
+		.img {
+			width: 100%;
+		}
+		.err-tit {
+			color: #666;
+			font-weight: bold;
+		}
+		.err-description {
+			margin-top: 20px;
+			text-align: center;
+			font-size: 14px;
+		}
+		.err-goback {
+			text-align: center;
+			color: #666;
+		}
+	}
 }
 </style>
